288fe7f3cf4bceb8e01fb75818c1f1c4043aabc8
NitroRiser <jonathanfariastech@gmail.com>
2022-09-06 12:34:48 +0000

    project created

---
+++ src/main/java/org/jtech/projectapimongodb/ProjectApiMongodbApplication.java
@@ -0,0 +1,13 @@
+package org.jtech.projectapimongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jtech/projectapimongodb/ProjectApiMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jtech.projectapimongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
