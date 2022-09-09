aaa2be0d0749833c62c6b3f191a157af7a0aca72
NitroRiser <jonathanfariastech@gmail.com>
2022-09-09 14:46:03 -0300

    Consulta simples com query methods

--- src/main/java/org/jtech/projectapimongodb/repository/PostRepository.java
+++ src/main/java/org/jtech/projectapimongodb/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package org.jtech.projectapimongodb.repository;
 
+import java.util.List;
+
 import org.jtech.projectapimongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
--- src/main/java/org/jtech/projectapimongodb/services/PostService.java
+++ src/main/java/org/jtech/projectapimongodb/services/PostService.java
@@ -1,5 +1,7 @@
 package org.jtech.projectapimongodb.services;
 
+import java.util.List;
+
 import org.jtech.projectapimongodb.domain.Post;
 import org.jtech.projectapimongodb.repository.PostRepository;
 import org.jtech.projectapimongodb.services.exceptions.ObjectNotFoundException;
@@ -9,18 +11,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class PostService {
 
-    @Autowired
-    private PostRepository repo;
-    
-    public Post findById(String id) {
-    	Post Post = repo.findById(id).orElse(null);
-    	if (Post == null) {
-    		throw new ObjectNotFoundException("Objeto não encontrado");
-    	}
-    	return Post;
-    }
+	@Autowired
+	private PostRepository repo;
 
-   
-    
-    
+	public Post findById(String id) {
+		Post Post = repo.findById(id).orElse(null);
+		if (Post == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return Post;
+	}
+
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
+
 }
